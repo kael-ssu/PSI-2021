e7c1e027189f9ece2ebaa3cffb440788e143699e
Kael Amir Pina Paraguassu Silva <kael.paraguassu@academico.ifrn.edu.br>
2021-06-16 20:55:04 -0300

    Primeira aplicação web (MVC) -  1.0

---
+++ Módulo I/PrimeiraAplicação (MVC)/PrimeiraAplicação/Controllers/CategoriasController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PrimeiraAplicação.Models;
+
+namespace PrimeiraAplicação.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria(){ CategoriaId = 1, Nome = "Notebooks" },
+            new Categoria(){ CategoriaId = 2, Nome = "Monitores" },
+            new Categoria(){ CategoriaId = 3, Nome = "Impressoras" },
+            new Categoria(){ CategoriaId = 4, Nome = "Mouses" },
+            new Categoria(){ CategoriaId = 5, Nome = "Desktops" }
+        };
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        // POST: Categorias
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
