16d57ad4814539a3994974c31f469f74f941ed44
Kael Amir Pina Paraguassu Silva <kael.paraguassu@academico.ifrn.edu.br>
2021-06-24 18:26:13 -0300

    Adição Banco de dados - primeira parte

---
+++ Módulo I/PrimeiraAplicação (MVC)/PrimeiraAplicação/Context/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using PrimeiraAplicação.Models;
+
+namespace PrimeiraAplicação.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ Módulo I/PrimeiraAplicação (MVC)/PrimeiraAplicação/Models/Fabricante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeiraAplicação.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+    }
+}
